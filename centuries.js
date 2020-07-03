53bc2f94edaf18888ab174a334d6a3e6c8bc076a
Monika Para <mpara3@illinois.edu>
2020-07-03 06:49:15 -0500

    Update centuries.js

--- centuries.js
+++ centuries.js
@@ -1,6 +1,22 @@
-function century() {
-  var year = prompt("Input a year");
-  return Math.floor((year-1)/100) + 1;
+function century(year) {
+  //testing for null if user does not submit response
+  if (year == null) {
+    return "please input a year"
+  } 
+  else {
+    //conditionals to determine what century the input gave
+    if (year >= 2000 && year <= 2099) {
+      var year = "21st";
+      } else if (year >= 1900 && year <= 1999) {
+        var year = "20th";
+      } else if (year >= 1800 && year <= 1899) {
+        var year = "19th";
+      } else {
+        return "you put" + year;
+      }
+  return year;
+}
 }
 
-century();
+//testing condition
+century(1998);
